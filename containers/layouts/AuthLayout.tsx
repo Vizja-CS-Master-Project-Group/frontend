0df04d66704b01a5f77d71ed85f94b8d2caf2dcb
melih <dev.mberatsanli@gmail.com>
2024-05-17 01:16:33 +0200

    auth layout title

--- containers/layouts/AuthLayout.tsx
+++ containers/layouts/AuthLayout.tsx
@@ -44,7 +44,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
-              <span className="">Acme Inc</span>
+              <span className="">The Library</span>
             </Link>
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
@@ -86,7 +86,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
                   className="flex items-center gap-2 text-lg font-semibold"
                 >
                   <Package2 className="h-6 w-6" />
-                  <span className="sr-only">Acme Inc</span>
+                  <span className="sr-only">The Library</span>
                 </Link>
                 <NavigationItem
                   href={"/"}
