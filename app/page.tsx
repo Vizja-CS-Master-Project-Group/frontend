e6fb615ac60f599c370fae6e560e2dda0ed5c5b7
melih <dev.mberatsanli@gmail.com>
2024-05-16 02:41:55 +0200

    next-auth interface adjusted

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,14 @@
-import Link from "next/link";
-import LoginForm from "@/components/auth/LoginForm";
-import {getServerSession} from "next-auth";
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import SignOutButton from "@/components/auth/SignOutButton";
 
 export default async function Home() {
-    const session = await getServerSession(authOptions);
-    console.log('session', session);
+  const session = await getServerSession(authOptions);
 
-    return (
-        <div>
-            {session && session.user && <SignOutButton />}
-            <LoginForm/>
-            <Link
-                href="api/auth/login"
-                className="text-grey-dark text-sm no-underline hover:text-grey-darker"
-            >
-                I already have an account
-            </Link>
-        </div>
-    );
+  return (
+    <div className={"p-4"}>
+      <div>{session && session.user && session.user.name}</div>
+      <SignOutButton />
+    </div>
+  );
 }
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,18 +1,21 @@
-import type { JWT, Session, User } from "next-auth/next"
+import type { JWT, Session, User } from "next-auth/next";
 
 declare module "next-auth" {
-    interface Session {
-        accessToken: string
-        user: {
-            id: string
-            accessToken: string
-        } & Session["user"]
-    }
-    interface User {
-        access_token: string
-    }
-    interface JWT {
-        access_token: string
-        user: {};
-    }
+  interface Session {
+    accessToken: string;
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      role: "librarian" | "user";
+      accessToken: string;
+    } & Session["user"];
+  }
+  interface User {
+    access_token: string;
+  }
+  interface JWT {
+    access_token: string;
+    user: {};
+  }
 }
