dd6e62a784df993cc9d39e58bb874201c1c564d4
melih <dev.mberatsanli@gmail.com>
2024-05-17 23:31:25 +0200

    env used

--- lib/api.ts
+++ lib/api.ts
@@ -1,18 +1,18 @@
 export function login(email?: string, password?: string, headers?: Headers) {
-    return fetch("http://backend.test/login", {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify({
-            email,
-            password
-        })
-    });
+  return fetch(`${process.env.BACKEND_API}/login`, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
 }
 
 export function csrfCookie() {
-    return fetch("http://backend.test/sanctum/csrf-cookie", {
-        method: "GET",
-    });
+  return fetch(`${process.env.BACKEND_API}/sanctum/csrf-cookie`, {
+    method: "GET",
+  });
 }
 
 export function backendApi() {}
