b927925592dcd609a4281033f5af816350fe9541
melih <dev.mberatsanli@gmail.com>
2024-06-02 02:10:08 +0200

    missed comp removed

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,9 +1,6 @@
-import SignOutButton from "@/components/auth/SignOutButton";
-
 export default function ForgotPassword() {
   return (
     <>
-      <SignOutButton />
       <div>Not Ready Yet :)</div>
     </>
   );
