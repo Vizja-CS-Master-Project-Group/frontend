ad613d4a4ab95853da85d15e01cdff016a9b0d1c
melih <dev.mberatsanli@gmail.com>
2024-06-14 01:36:15 +0200

    auth endpoints

--- app/actions/books.actions.ts
+++ app/actions/books.actions.ts
@@ -4,23 +4,12 @@ import { CreateBookFromSchema } from "@/containers/forms/book/book-create-form";
 import { ResourceActionProps } from "@/containers/resource/table-resource";
 import { ViewInterface, ViewResourceInterface } from "@/types/misc";
 import { BookInterface } from "@/types/book";
-import { post } from "@/lib/api";
+import { get, post } from "@/lib/api";
 
 export async function bookList(
   props: ResourceActionProps,
 ): Promise<ViewResourceInterface<BookInterface>> {
-  const response = await fetch(
-    `${process.env.BACKEND_API}/api/books?page=${props.page}`,
-    {
-      method: "GET",
-    },
-  );
-
-  if (!response.ok) {
-    throw new Error("Failed to get books");
-  }
-
-  return await response.json();
+  return get(`books?page=${props.page}`);
 }
 
 export async function bookCreate(
--- lib/api.ts
+++ lib/api.ts
@@ -2,11 +2,34 @@
 import getSession from "@/lib/getSession";
 import { redirect } from "next/navigation";
 
-export async function post<T>(path: string, body: any): Promise<T> {
+export async function get<T>(path: string): Promise<T> {
   const session = await getSession();
 
   if (!session) {
     // redirect("/login");
+  }
+
+  const response = await fetch(`${process.env.BACKEND_API}/api/${path}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${session?.user?.access_token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed when sending post request");
+  }
+
+  return response.json();
+}
+
+export async function post<T>(path: string, body: any): Promise<T> {
+  const session = await getSession();
+
+  if (!session) {
+    redirect("/login");
   }
 
   const response = await fetch(`${process.env.BACKEND_API}/api/${path}`, {
