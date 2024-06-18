1a303c20c83eed47431286d5695c5567868ff455
melih <dev.mberatsanli@gmail.com>
2024-06-18 13:26:05 +0200

    Update auth-dropdown.tsx

--- containers/navs/auth-dropdown.tsx
+++ containers/navs/auth-dropdown.tsx
@@ -12,6 +12,7 @@ import { Button } from "@/components/ui/button";
 import { CircleUser } from "lucide-react";
 import { signOut } from "next-auth/react";
 import { Session } from "next-auth";
+import Link from "next/link";
 
 export default function AuthDropdown({ session }: { session: Session }) {
   return (
@@ -25,8 +26,14 @@ export default function AuthDropdown({ session }: { session: Session }) {
       <DropdownMenuContent align="end">
         <DropdownMenuLabel>{session?.user.name}</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Settings</DropdownMenuItem>
-        <DropdownMenuItem>Support</DropdownMenuItem>
+        {session.user.role === "librarian" && (
+          <DropdownMenuItem asChild>
+            <Link href={"/settings"}>Settings</Link>
+          </DropdownMenuItem>
+        )}
+        <DropdownMenuItem asChild>
+          <Link href={"mailto:suppport@vizja.com"}>Support</Link>
+        </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem
           onClick={() => signOut({ callbackUrl: "/login", redirect: true })}
